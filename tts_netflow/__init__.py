49b5bcd5d74ce0eb75988158da3fdcc8a838e969
Peter Cacioppi <Peter.Cacioppi@gmail.com>
2021-04-11 16:51:12 -0700

    version bump and __all__ definition

--- tts_netflow/__init__.py
+++ tts_netflow/__init__.py
@@ -1,2 +1,4 @@
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 from tts_netflow.netflow import input_schema, solution_schema, solve
+# Some Python style guides argue that you shouldn't define __all__. Google coders do this.
+__all__ = ["input_schema", "solution_schema", "solve"]
